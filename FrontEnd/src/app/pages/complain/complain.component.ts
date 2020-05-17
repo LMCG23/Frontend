f4b67b233455939712c1081aa2c71117221b9afe
anthonyOviedo <antony-08@hotmail.com>
2020-05-16 21:25:07 -0600

    Editacion
    editar complain, editar news

--- FrontEnd/src/app/pages/complain/complain.component.ts
+++ FrontEnd/src/app/pages/complain/complain.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Complain, Department, Funcionario, Usuario } from 'src/app/models/model.index';
-import { ComplainService, UsuarioService } from 'src/app/services/service.index';
-import { toInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import Swal from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import {
+  Complain,
+  Department,
+  Funcionario,
+  Usuario
+} from "src/app/models/model.index";
+import {
+  ComplainService,
+  UsuarioService
+} from "src/app/services/service.index";
+import { toInteger } from "@ng-bootstrap/ng-bootstrap/util/util";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import Swal from "sweetalert2";
 declare var $: any;
 
 @Component({
-  selector: 'app-complain',
-  templateUrl: './complain.component.html',
-  styleUrls: ['./complain.component.css']
+  selector: "app-complain",
+  templateUrl: "./complain.component.html",
+  styleUrls: ["./complain.component.css"]
 })
 export class ComplainComponent implements OnInit {
   closeResult: string;
@@ -18,8 +26,8 @@ export class ComplainComponent implements OnInit {
   departmens: Department[] = [];
   complains: Complain[] = [];
   funcionarios: Funcionario[] = [];
-  department_Id: string = '-1';
-  depSeleccion: string = '';
+  department_Id: string = "-1";
+  depSeleccion: string = "";
   funcioSeleccion: number = 0;
   usuario: Usuario;
   fecha: Date;
@@ -27,12 +35,10 @@ export class ComplainComponent implements OnInit {
   Funcio: Funcionario;
   Description: string;
 
-
-  desde: string = '';
-  hasta: string = '';
-  Complainstate: string = '-1';
-  departmentid_Filter: string = '-1';
-
+  desde: string = "";
+  hasta: string = "";
+  Complainstate: string = "-1";
+  departmentid_Filter: string = "-1";
 
   // Pagination
   page: number = 1;
@@ -52,49 +58,45 @@ export class ComplainComponent implements OnInit {
 
   start() {
     this.fecha = new Date();
-    this.Funcio = new Funcionario('', '', '', -1);
-    this.fechaString = (this.fecha.getDate() + "/" + (this.fecha.getMonth() + 1) + "/" + this.fecha.getFullYear());
-    this.queja = new Complain(-1, '', '', -1, -1, '', '', -1, '');
+    this.Funcio = new Funcionario("", "", "", -1);
+    this.fechaString =
+      this.fecha.getDate() +
+      "/" +
+      (this.fecha.getMonth() + 1) +
+      "/" +
+      this.fecha.getFullYear();
+    this.queja = new Complain(-1, "", "", -1, -1, "", "", -1, "");
     this.list();
 
-    this._complainService.ListaDepartamentos()
-      .subscribe(result => {
-
-        var departmens = result.departamentos;
-        this.departmens = departmens;
-        console.log(this.departmens);
-
-      })
-
-
+    this._complainService.ListaDepartamentos().subscribe(result => {
+      var departmens = result.departamentos;
+      this.departmens = departmens;
+      console.log(this.departmens);
+    });
   }
-
-
 
   list() {
-
-    this._complainService.complaintsFilter(this.desde, this.hasta, this.Complainstate, this.departmentid_Filter)
+    this._complainService
+      .complaintsFilter(
+        this.desde,
+        this.hasta,
+        this.Complainstate,
+        this.departmentid_Filter
+      )
       .subscribe(result => {
         this.complains = result.complaints;
-
       });
-
   }
 
-
   buscarEmployee() {
-
-
-    this._complainService.ListaFuncionarios(this.depSeleccion)
+    this._complainService
+      .ListaFuncionarios(this.depSeleccion)
       .subscribe(result => {
-
         var funcionarios = result.funcionarios;
         this.funcionarios = funcionarios;
         console.log(this.funcionarios);
-
-      })
-    $('#funcionarioSearch').modal('show');
-
+      });
+    $("#funcionarioSearch").modal("show");
   }
 
   capturar() {
@@ -104,179 +106,149 @@ export class ComplainComponent implements OnInit {
     //https://stackblitz.com/edit/angular-dsrx92?file=app%2Fapp.component.html
   }
 
-
   agregarFuncionario(item: Funcionario) {
-
     this.Funcio = item;
 
-    $('#funcionarioSearch').modal('toggle');
+    $("#funcionarioSearch").modal("toggle");
   }
 
-
-  EditComplain(item: Complain) {
-    $('html, body').animate({
-      scrollTop: $('#titulo').offset().top
-    }, 1200);
+  EditComplain(item: any) {
+    // $("html, body").animate(
+    //   {
+    //     scrollTop: $("#titulo").offset().top
+    //   },
+    //   1200
+    // );
 
     this.queja = Object.assign({}, item);
     this.department_Id = this.queja.department_id.toString();
   }
 
-
   Validate(Complain: Complain) {
-    if (Complain.department_id == -1)
-      return 'Debe seleccionar el departamento';
+    if (Complain.department_id == -1) return "Debe seleccionar el departamento";
 
-    if (Complain.employee_name == ' ')
-      return 'Debe seleccionar el funcionario';
+    if (Complain.employee_name == " ") return "Debe seleccionar el funcionario";
 
-    if (Complain.Description == '')
-      return 'Debe ingresar una descripción';
+    if (Complain.Description == "") return "Debe ingresar una descripción";
 
-    return '';
-
-
+    return "";
   }
-
 
   enviar() {
     if (this.queja.Complain_Id == -1) {
       this.queja.department_id = parseInt(this.depSeleccion);
       this.queja.employee = this.Funcio.Person_Id.toString();
-      this.queja.state = 'Pendiente';
+      this.queja.state = "Pendiente";
       this.queja.User_id = this.usuario.usuario_Id;
       this.queja.person_Id = this.usuario.persona.persona_Id;
-      this.queja.employee_name = this.Funcio.nombre + ' ' + this.Funcio.apellido1;
+      this.queja.employee_name =
+        this.Funcio.nombre + " " + this.Funcio.apellido1;
       this.queja.fecha = this.fechaString;
 
-      console.log(this.queja );
+      console.log(this.queja);
       let msj = this.Validate(this.queja);
-      
-      if (msj != '') {
-        Swal.fire('Error', msj, 'error');
+
+      if (msj != "") {
+        Swal.fire("Error", msj, "error");
         return;
       } else {
-        this._complainService.GuardarQueja(this.queja)
-          .subscribe(
-            result => {
-              this.list();
-              this.start();
-            }
-
-          );
+        this._complainService.GuardarQueja(this.queja).subscribe(result => {
+          this.list();
+          this.start();
+        });
       }
-
-
-
-    }
-    else {
-
+    } else {
       this.queja.department_id = parseInt(this.depSeleccion);
       this.queja.employee = this.Funcio.Person_Id.toString();
-      this.queja.employee_name = this.Funcio.nombre + ' ' + this.Funcio.apellido1;
+      this.queja.employee_name =
+        this.Funcio.nombre + " " + this.Funcio.apellido1;
       this.queja.fecha = this.fechaString;
 
-
-      
       let msj = this.Validate(this.queja);
 
-      if (msj != '') {
-        Swal.fire('Error', msj, 'error');
+      if (msj != "") {
+        Swal.fire("Error", msj, "error");
         return;
       } else {
-        this._complainService.UpdateComplain(this.queja)
-          .subscribe(result => {
-            this.list();
-            this.start();
-
-          });
-
+        this._complainService.UpdateComplain(this.queja).subscribe(result => {
+          this.list();
+          this.start();
+        });
       }
-
-
-
-
     }
-
   }
 
   CargaXUsuario() {
-    this._complainService.ListComplainsbyId(this.usuario.usuario_Id)
+    this._complainService
+      .ListComplainsbyId(this.usuario.usuario_Id)
       .subscribe(result => {
-
         var complains = result.complains;
         this.complains = complains;
-
-
-      })
-
+      });
   }
 
   cleanComplain() {
-    this.queja = new Complain(-1, '', '', -1, -1, '', '', -1, '');
-    this.queja.Description = '';
-    this.Funcio.nombre = '';
-    this.department_Id = '-1';
-
+    this.queja = new Complain(-1, "", "", -1, -1, "", "", -1, "");
+    this.queja.Description = "";
+    this.Funcio.nombre = "";
+    this.department_Id = "-1";
   }
   deleteComplain(item: Complain) {
-    if (item.state == 'Pendiente') {
-      this._complainService.DeleteComplain(item.Complain_Id)
+    if (item.state == "Pendiente") {
+      this._complainService
+        .DeleteComplain(item.Complain_Id)
         .subscribe(result => {
           this.CargaXUsuario();
         });
     } else {
-      Swal.fire('Error de validación', 'La queja ya fue vista por el departamento', 'error');
+      Swal.fire(
+        "Error de validación",
+        "La queja ya fue vista por el departamento",
+        "error"
+      );
     }
-
-
-
   }
 
   complaindatail(item: any) {
-    this.Description = item.Description
-    $('#complaindatail').modal('show');
-
+    this.Description = item.Description;
+    $("#complaindatail").modal("show");
   }
 
-
-
   open(content) {
-
-    if (this.department_Id == '-1') {
-      Swal.fire('Error', 'Debe seleccionar un departamento', 'error');
+    if (this.department_Id == "-1") {
+      Swal.fire("Error", "Debe seleccionar un departamento", "error");
       return;
     }
 
-    this._complainService.ListaFuncionarios(this.depSeleccion)
+    this._complainService
+      .ListaFuncionarios(this.depSeleccion)
       .subscribe(result => {
-
         var funcionarios = result.funcionarios;
         this.funcionarios = funcionarios;
+      });
 
-
+    this.modalService
+      .open(content, {
+        ariaLabelledBy: "modal-basic-title",
+        windowClass: "myCustomModalClass"
       })
-
-
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', windowClass: "myCustomModalClass" }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+      .result.then(
+        result => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        reason => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
+      return "by pressing ESC";
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
+      return "by clicking on a backdrop";
     } else {
       return `with: ${reason}`;
     }
   }
-
-
-
-
-
 }
--- FrontEnd/src/app/pages/news/news.component.ts
+++ FrontEnd/src/app/pages/news/news.component.ts
@@ -1,41 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { News} from 'src/app/models/model.index';
-import { NgForm } from '@angular/forms';
-import {  NewService } from 'src/app/services/service.index';
-import Swal from 'sweetalert2';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from "@angular/core";
+import { News } from "src/app/models/model.index";
+import { NgForm } from "@angular/forms";
+import { NewService } from "src/app/services/service.index";
+import Swal from "sweetalert2";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 declare var $: any;
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  selector: "app-news",
+  templateUrl: "./news.component.html",
+  styleUrls: ["./news.component.css"]
 })
 export class NewsComponent implements OnInit {
   closeResult: string;
 
-
-  url: string = 'http://placehold.it/180';
+  url: string = "http://placehold.it/180";
   Noticia: News;
 
   listNews: News[] = [];
   ingresando: boolean = false;
   message: string;
-  hasta:string='';
-  filtro:string='';
-  descripcion:string = '';
-  fileToUpload:string = ''
-  titulo:string ='';
+  hasta: string = "";
+  filtro: string = "";
+  descripcion: string = "";
+  fileToUpload: string = "";
+  titulo: string = "";
 
   //pagination
-  page:number = 1;
-  pageSize:number = 10;
-
+  page: number = 1;
+  pageSize: number = 10;
 
   constructor(
     private _NewService: NewService,
     private modalService: NgbModal
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.inicio();
@@ -43,163 +41,153 @@ export class NewsComponent implements OnInit {
 
   inicio() {
     this.GetAllNews();
-    this.Noticia = new News( '', '', '','');
-    this.Noticia.fileToUpload = 'http://placehold.it/180';
+    this.Noticia = new News("", "", "", "");
+    this.Noticia.fileToUpload = "http://placehold.it/180";
   }
 
-
-
   ingresar(form: NgForm) {
-
     if (form.invalid) {
       return;
     }
- 
+
     this.message = this.validar();
-    if (this.message == '') {
-    this.ingresando = true;
-    this._NewService.postFile(this.Noticia)
-      .subscribe( result =>{
-        this.url= 'http://placehold.it/180';
+    if (this.message == "") {
+      this.ingresando = true;
+      this._NewService.postFile(this.Noticia).subscribe(result => {
+        this.url = "http://placehold.it/180";
         this.ingresando = false;
-        
+
         this.GetAllNews();
         this.inicio();
-      
-      } );
-
-    
-
-
-    }
-    else{
-      Swal.fire('Error de validación', this.message, 'error');
+      });
+    } else {
+      Swal.fire("Error de validación", this.message, "error");
     }
   }
 
+  GetAllNews() {
+    this._NewService
+      .ObtenerNoticiasFiltro(this.hasta, this.filtro)
+      .subscribe(result => {
+        this.listNews = result.Noticias;
+        console.log(this.listNews);
+      });
+  }
 
-  GetAllNews(){
-    this._NewService.ObtenerNoticiasFiltro(this.hasta,this.filtro)
-    .subscribe(result => {
-      this.listNews = result.Noticias;
-      console.log(this.listNews);
-      
+  deletenew(item: any) {
+    this._NewService.Delete(item.new_id).subscribe(result => {
+      this.inicio();
     });
   }
 
-  deletenew(item:any){
-    this._NewService.Delete(item.new_id)
-    .subscribe(result => {
-      this.inicio();
-    } );
-  
+  Detail(item: any) {
+    this.descripcion = item.descripcion;
+    this.fileToUpload = item.fileToUpload;
   }
 
-  Detail(item:any){
-    this.descripcion = item.descripcion
-    this.fileToUpload = item.fileToUpload
-
-  }
-
-  EditNew(item:any){
-    this.Noticia = Object.assign({},item);
-    this.descripcion =   Object.assign({},this.Noticia.descripcion);
-    this.titulo = Object.assign({},this.Noticia.titulo);
+  EditNew(item: any) {
+    //como cargar la fecha de caducidad
+    this.Noticia = Object.assign({}, item);
+    this.descripcion = Object.assign({}, this.Noticia.descripcion);
+    this.titulo = Object.assign({}, this.Noticia.titulo);
     this.url = this.Noticia.fileToUpload;
 
-    $('html, body').animate({
-      scrollTop: $('#titulo').offset().top
-    }, 1200);
+    $("html, body").animate(
+      {
+        scrollTop: $("#titulo").offset().top
+      },
+      1200
+    );
   }
 
+  // muestra el modal
+  AddPhoto() {
+    $("#addphoto").modal("show");
+  }
+  // fuente de la imagen
+  GuardarFotos() {
+    $("#addphoto").modal("toggle");
+  }
 
-
-// muestra el modal
-AddPhoto(){
-  $('#addphoto').modal('show');
-}
-// fuente de la imagen
-GuardarFotos(){
-  $('#addphoto').modal('toggle');
-}
-
-// cierra el modal o lo abre 
-readUrl(event:any) {
-  if (event.target.files && event.target.files[0]) {
+  // cierra el modal o lo abre
+  readUrl(event: any) {
+    if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
       var sa = event.target.files;
       var fileName = sa[0].type;
       var archivo2;
-      console.log("url normal=" +fileName);
-      
-      reader.onload = (event:any) => {
-       
-          this.Noticia.fileToUpload = event.target.result;
-          
-           }
+      console.log("url normal=" + fileName);
+
+      reader.onload = (event: any) => {
+        this.Noticia.fileToUpload = event.target.result;
+      };
 
       reader.readAsDataURL(event.target.files[0]);
-      
-     
-
+    }
   }
-}
 
-validar() {
-  if (this.Noticia.descripcion == '')
-    return 'Debe ingresar una descripcion';
-    
-  if (this.Noticia.titulo == '')
-    return 'Debe ingresar un titulo';
+  validar() {
+    if (this.Noticia.descripcion == "") return "Debe ingresar una descripcion";
 
-    if (this.Noticia.fileToUpload == 'http://placehold.it/180')
-    return 'Debe insertar una imagen';
-  // if (this.Persona.strId == '')
-  //   this.errormsj = 'Debe el ingresar el número de cedula';
-  return '';
-}
+    if (this.Noticia.titulo == "") return "Debe ingresar un titulo";
 
+    if (this.Noticia.fileToUpload == "http://placehold.it/180")
+      return "Debe insertar una imagen";
+    // if (this.Persona.strId == '')
+    //   this.errormsj = 'Debe el ingresar el número de cedula';
+    return "";
+  }
 
-   open(content) {
-      this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', windowClass : "myCustomModalClass"  }).result.then((result) => {
-        this.closeResult = `Closed with: ${result}`;
-      }, (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      });
+  open(content) {
+    this.modalService
+      .open(content, {
+        ariaLabelledBy: "modal-basic-title",
+        windowClass: "myCustomModalClass"
+      })
+      .result.then(
+        result => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        reason => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
     }
+  }
 
-    private getDismissReason(reason: any): string {
-      if (reason === ModalDismissReasons.ESC) {
-        return 'by pressing ESC';
-      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-        return 'by clicking on a backdrop';
-      } else {
-        return  `with: ${reason}`;
-      }
+  open2(content, item: any) {
+    this.Detail(item);
+    this.modalService
+      .open(content, {
+        ariaLabelledBy: "modal-basic-title",
+        windowClass: "myCustomModalClass"
+      })
+      .result.then(
+        result => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        reason => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason2(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
     }
-
-
-    open2(content,item:any) {
-      this.Detail(item);
-      this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', windowClass : "myCustomModalClass"  }).result.then((result) => {
-        this.closeResult = `Closed with: ${result}`;
-      }, (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      });
-    }
-
-    private getDismissReason2(reason: any): string {
-      if (reason === ModalDismissReasons.ESC) {
-        return 'by pressing ESC';
-      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-        return 'by clicking on a backdrop';
-      } else {
-        return  `with: ${reason}`;
-      }
-    }
-
-
-
-
-
+  }
 }
